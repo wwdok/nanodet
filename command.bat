3ad948aa3431d5e1493454acbc968d8d7f75ece8
wwdok <wade.wang96@outlook.com>
2021-03-23 11:23:34 +0800

    improve training expirience

---
+++ command.bat
@@ -0,0 +1,3 @@
+# train
+python tools/train.py config/nanodet_m_320_special_vehicle_9classes.yml
+python tools/train.py config/EfficientNet-Lite/nanodet-EfficientNet-Lite0_320_special_vehicle_9classes.yml
